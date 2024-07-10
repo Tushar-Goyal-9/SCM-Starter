1bc9c1ef24df73c4e11506061187c3b5b0e8c012
Tushar <tushargoyal012345@gmail.com>
2024-07-10 15:01:40 +0530

    d

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import voting_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+import assessment_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
@@ -13,7 +13,7 @@ export default function HomePage() {
   const [isClient, setIsClient] = useState(false);
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
-  const votingABI = voting_abi.abi;
+  const votingABI = assessment_abi.abi; // Updated variable name to match the import
 
   useEffect(() => {
     setIsClient(true);
