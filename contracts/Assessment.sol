f44e8c4cf82d5c4e29fa010e83ea7365586c88ad
Tushar <tushargoyal012345@gmail.com>
2024-07-10 15:30:55 +0530

    d

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -15,7 +15,7 @@ export default function HomePage() {
   const [lockDuration, setLockDuration] = useState("");
   const [isClient, setIsClient] = useState(false);
 
-  const contractAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = assessment_abi.abi;
 
   useEffect(() => {
